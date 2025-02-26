32b199f93b1545ea4f8a99f4df7944f270c99ca5
Syed Saqlain Wali Shah <161953058+belaposh@users.noreply.github.com>
2025-02-26 13:29:52 +0500

    Add files via upload

---
+++ Practical 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"enter the number from 1 to 3 to select daily internet packages \n";
+	cin>>a;
+	switch(a){
+		    case 1:
+			cout<<"Daily 2 Gb";
+			break;
+			
+			case 2:
+			cout<<"Daily 4 Gb";
+			break;
+			
+	        case 3:
+			cout<<"Daily 6 Gb";
+			break;
+	
+	
+	}
+	
+}
